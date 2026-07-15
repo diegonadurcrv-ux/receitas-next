55cdf30fb9e746ce06ec46f7a518458c6c18e1c0
Diego Nadur <diegonadurcrv@gmail.com>
2026-07-15 01:40:39 -0300

    feat: adiciona responsividade ao layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,9 +11,9 @@ export default function Home() {
 
     {/* seção Hero*/}
     <section className=" bg-orange-50 py-12">
-      <div className="flex flex-col gap-6 items-center container mx-auto">
-        <h1 className="text-5xl font-bold">Receitas deliciosas</h1>
-        <p className="text-xl">Descubra receitas simples e sabororsas para todas as ocasiões</p>
+      <div className="flex flex-col gap-6 items-center container mx-auto px-4">
+        <h1 className="text-3xl sm:text-5xl font-bold text-center">Receitas deliciosas</h1>
+        <p className="text-lg sm:text-xl text-center">Descubra receitas simples e sabororsas para todas as ocasiões</p>
 
         <Link className="bg-orange-500 hover:bg-orange-700 transition-colors text-white font-bold rounded-lg px-3 py-2" href="/receitas" >
           Ver todas as receitas
@@ -23,10 +23,10 @@ export default function Home() {
 
     {/* seção receitas em destaque */}
     <section className="py-12">
-      <div className="flex flex-col items-center container mx-auto gap-8">
+      <div className="flex flex-col items-center container mx-auto gap-8 px-4">
         <h2 className="text-lg font-bold">Receitas em destaque</h2>
 
-        <div className="flex w-full gap-8">
+        <div className="flex flex-col sm:flex-row w-full gap-8">
           {featuredRecipes.map((recipe) => (
             <RecipeCard key={recipe.id} recipe={recipe}/>
           ))}
