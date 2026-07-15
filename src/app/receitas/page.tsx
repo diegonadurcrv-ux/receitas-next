f2862c5cd2fc83a2ff9dce6f59489d57245fcbd8
Diego Nadur <diegonadurcrv@gmail.com>
2026-07-14 22:49:34 -0300

    feat: estrutura da pagina de receitas

---
+++ src/app/receitas/page.tsx
@@ -0,0 +1,13 @@
+export default function Receitaspage() {
+    return (
+        <main className="grow py-8">
+            <div className="container mx-auto">
+                <h1 className="text-3xl font bold">Todas as receitas</h1>
+
+                <div>
+                    {/* array de cards de receitas */}
+                </div>
+            </div>
+        </main>
+    )
+}
